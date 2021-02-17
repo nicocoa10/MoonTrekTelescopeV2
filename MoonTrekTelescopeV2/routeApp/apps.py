384033fb073167307aeadbba984083a7a6d53278
nicocoa10 <nojeda3@calstatela.edu>
2021-02-17 14:23:32 -0800

    set up initial files of project and urls , settings , views

---
+++ MoonTrekTelescopeV2/routeApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RouteappConfig(AppConfig):
+    name = 'routeApp'
---
+++ MoonTrekTelescopeV2/routeApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def upload (request):
+
+    return render(request,'routeApp/upload.html')
+
